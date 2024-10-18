a837d28ad65aa2aff27fe21f068d87295f4bb101
beshoy_younan <beshoy.younan@outlook.com>
2024-10-18 17:56:25 -0300

    Initial commit

---
+++ src/main/java/com/besho/restapi/crm_api/CrmManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.besho.restapi.crm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/besho/restapi/crm_api/CrmManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besho.restapi.crm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
